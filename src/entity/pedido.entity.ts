0e7354ea52ce783c839fcd506ebb8cd161ede4cc
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-07-04 21:47:19 -0300

    Edição e entidade; criação enum.

--- src/entity/pedido.entity.ts
+++ src/entity/pedido.entity.ts
@@ -1,1 +1,30 @@
-export class Pedido {}
+import { StatusPedido } from "src/pedido/enum/status-pedido.enum";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn } from "typeorm"
+
+@Entity({ name: 'pedidos' })
+export class PedidoEntity {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ name: 'valor_total', nullable: false })
+  valorTotal: number;
+
+  @Column({ name: 'status', enum: StatusPedido, nullable: false })
+  status: StatusPedido;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
