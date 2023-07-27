d536f9dc93cdfc37bcca5508e506eff494ebfa8c
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-07-27 17:23:12 -0300

    Removendo import inutilizado.

--- src/pedido/pedido.module.ts
+++ src/pedido/pedido.module.ts
@@ -4,7 +4,6 @@ import { PedidoController } from './pedido.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PedidoEntity } from '../entity/pedido.entity';
 import { UsuarioEntity } from '../entity/Usuario.entity';
-import { ItemPedidoEntity } from '../entity/item-pedido.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PedidoEntity, UsuarioEntity])],
